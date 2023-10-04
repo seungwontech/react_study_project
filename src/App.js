8273d540a894646186e7c8e87f135e19e69e95a7
sw.lee <swlee173@gmail.com>
2023-10-04 17:43:31 +0900

    컴포넌트만들기1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello React
+    <div>
+        <header>
+            <h1><a href="/">WEB</a></h1>
+        </header>
+        <nav>
+            <ol>
+                <li>
+                    <a href="/">html</a>
+                </li>
+                <li>
+                    <a href="/">css</a>
+                </li>
+                <li>
+                    <a href="/">js</a>
+                </li>
+            </ol>
+        </nav>
+        <article>
+            <h2>welcome</h2>
+        </article>
     </div>
   );
 }
