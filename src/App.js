f84a8b555f4bba78c5c4a3729878962d653a593e
sw.lee <swlee173@gmail.com>
2023-10-04 17:49:15 +0900

    컴포넌트만들기2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-
+function Header(){
+    return <header>
+        <h1><a href="/">WEB</a></h1>
+    </header>
+}
+function Nav(){
+    return <nav>
+        <ol>
+            <li>
+                <a href="/">html</a>
+            </li>
+            <li>
+                <a href="/">css</a>
+            </li>
+            <li>
+                <a href="/">js</a>
+            </li>
+        </ol>
+    </nav>
+}
+function Article(){
+    return <h2>welcome</h2>
+}
 function App() {
   return (
     <div>
-        <header>
-            <h1><a href="/">WEB</a></h1>
-        </header>
-        <nav>
-            <ol>
-                <li>
-                    <a href="/">html</a>
-                </li>
-                <li>
-                    <a href="/">css</a>
-                </li>
-                <li>
-                    <a href="/">js</a>
-                </li>
-            </ol>
-        </nav>
-        <article>
-            <h2>welcome</h2>
-        </article>
+        <Header></Header>
+        <Nav></Nav>
+        <Article></Article>
     </div>
   );
 }
