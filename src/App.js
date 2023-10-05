b489b5d8efac93a2ea89d3f5fdec184346bec021
sw.lee <swlee173@gmail.com>
2023-10-05 17:43:46 +0900

    props3

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-function Header(props){
+
+function Header(props) {
     return <header>
         <h1><a href="/">{props.title}</a></h1>
     </header>
 }
-function Nav(){
+
+function Nav(props) {
+    const lis = []
+    for(let i = 0; i < props.topics.length; i++){
+        let t = props.topics[i];
+        lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+        /**
+         *  key를 사용하는 이유
+         *  - 컴포넌트의 변화를 감지하기 위해
+         *  - 안정적인 고유성을 부여하기 위해
+        */
+    }
     return <nav>
         <ol>
-            <li>
-                <a href="/">html</a>
-            </li>
-            <li>
-                <a href="/">css</a>
-            </li>
-            <li>
-                <a href="/">js</a>
-            </li>
+            {lis}
         </ol>
     </nav>
 }
-function Article(props){
+
+function Article(props) {
     return <h2>{props.title}, {props.body}</h2>
 }
+
 function App() {
-  return (
-    <div>
-        <Header title="REACT"></Header>
-        <Nav></Nav>
-        <Article title="Welcome" body="Hello, WEB"></Article>
-        <Article title="Welcome react" body="Hi"></Article>
-    </div>
-  );
+    const topics = [
+        {id:1, title:'html', body:'html is...'},
+        {id:2, title:'css', body:'css is ...'},
+        {id:3, title:'js', body:'js is...'}
+    ]
+    return (
+        <div>
+            <Header title="REACT"></Header>
+            <Nav topics={topics}></Nav>
+            <Article title="Welcome" body="Hello, WEB"></Article>
+        </div>
+    );
 }
 
 export default App;
