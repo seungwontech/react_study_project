8d5036ed543b6a11cf6b1430edc592a40a508754
sw.lee <swlee173@gmail.com>
2023-10-12 16:34:40 +0900

    Create

--- src/App.js
+++ src/App.js
@@ -38,6 +38,22 @@ function Article(props) {
     return <h2>{props.title}, {props.body}</h2>
 }
 
+function Create(props) {
+    return <article>
+        <h2>Create</h2>
+        <form onSubmit={event => {
+            event.preventDefault();
+            const title = event.target.title.value;
+            const body = event.target.body.value;
+            props.onCreate(title, body)
+        }}>
+            <p><input type="text" name="title" placeholder="title"/></p>
+            <p><textarea name="body" placeholder="body"></textarea></p>
+            <p><input type="submit" value="Create"/></p>
+        </form>
+    </article>
+}
+
 function App() {
     /**
      * const _mode = useState('WELCOME');
@@ -49,23 +65,36 @@ function App() {
      */
     const [mode, setMode] = useState('WELCOME');
     const [id, setId] = useState(null);
-    const topics = [
+    const [nextId, setNextId] = useState(4);
+
+    const [topics, setTopics] = useState([
         {id: 1, title: 'html', body: 'html is...'},
         {id: 2, title: 'css', body: 'css is ...'},
         {id: 3, title: 'js', body: 'js is...'}
-    ]
+    ]);
+
     let content = null;
     if (mode === 'WELCOME') {
         content = <Article title="Welcome" body="Hello, WEB"></Article>
     } else if (mode === 'READ') {
         let title, body = null;
-        for(let i =0; i<topics.length; i++){
-            if(topics[i].id === id){
+        for (let i = 0; i < topics.length; i++) {
+            if (topics[i].id === id) {
                 title = topics[i].title;
                 body = topics[i].body;
             }
         }
         content = <Article title={title} body={body}></Article>
+    } else if (mode === 'CREATE') {
+        content = <Create onCreate={(_title, _body) => {
+            const newTopic = {id: nextId, title: _title, body: _body}
+            const newTopics = [...topics] // 원본을 복제
+            newTopics.push(newTopic); // 새로운값을 추가
+            setTopics(newTopics); // 세팅
+            setMode('READ');
+            setId(nextId);
+            setNextId(nextId + 1);
+        }}></Create>
     }
     return (
         <div>
@@ -77,6 +106,10 @@ function App() {
                 setId(_id);
             }}></Nav>
             {content}
+            <a href="/create" onClick={event => {
+                event.preventDefault();
+                setMode('CREATE');
+            }}>Create</a>
         </div>
     );
 }
